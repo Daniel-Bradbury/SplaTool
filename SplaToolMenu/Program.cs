34cd44a0af242e98ab073d9eb89f91367ffe118b
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-04-17 17:06:06 +0100

    Added menu test

---
+++ SplaToolMenu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace SplaToolMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread MouseThread = new Thread(Mouse.MouseThread);
+            MouseThread.Start();
+            for (; ; )
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Splatools v0.3");
+                Console.WriteLine("X:" + Mouse.X + " Y:"+ Mouse.Y + " Status:" + Mouse.Status + "      ");
+            }
+        }
+    }
+}
