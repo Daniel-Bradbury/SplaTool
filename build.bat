14d9ca67687914f7bb8aa9ad237656b688e77db7
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-04-17 19:44:25 +0100

    Made name consistant to 'SplaTools'

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 echo Initialising...
 rd /S /Q "%cd%\BUILD"
 echo Compiling binaries...
-start /B csc -out:SplaTool.exe SplaToolMenu\*.cs
+start /B csc -out:SplaTools.exe SplaToolsMenu\*.cs
 processing-java --sketch="%cd%\FestTool" --no-java --export --platform windows
 echo Moving binaries into build folder...
 cd FestTool
@@ -16,8 +16,8 @@ mkdir 32
 cd..
 move FestTool\FestTool32 BUILD\32\FestTool
 move FestTool\FestTool64 BUILD\64\FestTool
-copy SplaTool.exe BUILD\32
-move SplaTool.exe BUILD\64
+copy SplaTools.exe BUILD\32
+move SplaTools.exe BUILD\64
 echo Cleaning up...
 rd /S /Q "%cd%\BUILD\32\FestTool\source"
 rd /S /Q "%cd%\BUILD\64\FestTool\source"
