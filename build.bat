af3c2bf63a9bb8b6858f22be648ec06b087d0ce2
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-04-18 18:40:41 +0100

    Made Zoom.exe compile to 32x AND 64x

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ rd /S /Q "%cd%\BUILD"
 echo Compiling binaries...
 start /B csc -out:SplaTools.exe SplaToolsMenu\*.cs >nul
 processing-java --sketch="%cd%\FestTool" --no-java --export --platform windows >nul
-"c:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "SplaToolsMenu\Zoom.ahk"
+"c:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "SplaToolsMenu\Zoom.ahk" /out "SplaToolsMenu\Zoom32.exe" /bin "c:\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin"
+"c:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "SplaToolsMenu\Zoom.ahk" /out "SplaToolsMenu\Zoom64.exe" /bin "c:\Program Files\AutoHotkey\Compiler\Unicode 64-bit.bin"
 echo Moving binaries into build folder...
 cd FestTool
 ren application.windows32 FestTool32
@@ -25,8 +26,10 @@ move FestTool\FestTool32 BUILD\SplaTools32\FestTool
 move FestTool\FestTool64 BUILD\SplaTools64\FestTool
 copy SplaTools.exe BUILD\SplaTools32
 move SplaTools.exe BUILD\SplaTools64
-copy SplaToolsMenu\Zoom.exe BUILD\SplaTools32\data
-move SplaToolsMenu\Zoom.exe BUILD\SplaTools64\data
+move SplaToolsMenu\Zoom32.exe BUILD\SplaTools32\data
+move SplaToolsMenu\Zoom64.exe BUILD\SplaTools64\data
+ren BUILD\SplaTools64\data\Zoom64.exe Zoom.exe
+ren BUILD\SplaTools32\data\Zoom32.exe Zoom.exe
 echo Cleaning up...
 rd /S /Q "%cd%\BUILD\SplaTools32\FestTool\source"
 rd /S /Q "%cd%\BUILD\SplaTools64\FestTool\source"
